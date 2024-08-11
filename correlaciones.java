4692d3f9567fe3c626e36d9a899dcbe9613a8938
FORZASPYKE <badillafimbresluis@gmail.com>
2024-08-10 17:51:39 -0700

    Se agrega el proyecto

---
+++ correlaciones.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class correlaciones {
+    public static void main(String[] args){
+         try{
+            FileReader ArchivoLectura = new FileReader("startup-profit.csv");
+
+            BufferedReader ArchivoEscritura = new BufferedReader(startup-profit.csv);
+            String almacen = null;
+            double suma = 0.0;
+            int contador = 0;
+         }
+
+    }
+}
